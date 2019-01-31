a27a4919c36bba89865108e916a57fb3e2408337
lsb <1731976958@qq.com>
2019-01-31 23:28:05 +0800

    增加图片和标题

--- gallery/models.py
+++ gallery/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Gallery(models.Model):
-	description = models.CharField(max_length=100)
-   
-    
+	description = models.CharField(default='在这里写作品简介', max_length=100)
+	imge = models.ImageField(default='default.png',upload_to='imges/')   	
+	title = models.CharField(default='作品标题', max_length=50)	  	
+   	#在admin中显示标题
+	def _str_(self):
+		return self.title
